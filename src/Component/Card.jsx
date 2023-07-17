93e2dfff9f96bbc1c2b1b7309503384bdeff29d2
Raphael Isla <rpbisla0025@gmail.com>
2023-07-17 21:55:21 +0800

    responsive touch

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -24,7 +24,7 @@ function Card({icon, title, content}) {
     const cardClasses = `card custom-card p-4-xl p-0 w-100 p-2-lg text-wrap animate__animated animate__fadeIn border-0  ${
         isSmallScreen ? '' : isHovered ? '' : 'bg-transparent'}`;
     return (
-        <div className={cardClasses} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{backgroundColor: isSmallScreen ? 'var(--tertiary-color)' : 'transparent'}}>
+        <div className={cardClasses} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{backgroundColor: isSmallScreen ? 'var(--tertiary-color)' : ''}}>
             <div className='card-icon service-color d-flex justify-content-center align-items-center mt-3 mb-3'>{icon}</div>
             <div className='card-body'>
                 <h4 className='service-color fw-bold text-uppercase'>{title}</h4>
